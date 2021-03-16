3ac9ab8fa75347896239bba6275763e256f9279f
retr00h <34344402+retr00h@users.noreply.github.com>
2021-03-16 17:17:27 +0100

    Initial commit.

---
+++ app/src/main/java/com/devfabiocirelli/spaceinvaders/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.devfabiocirelli.spaceinvaders
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
