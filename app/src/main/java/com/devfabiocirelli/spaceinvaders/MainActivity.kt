290c72e7770b7ff78b1c93174c6f0fa63a5f136a
retr00h <34344402+retr00h@users.noreply.github.com>
2021-03-16 17:20:46 +0100

    Aggiunto plugin 'kotlin-android-extensions' in build.gradle; aggiunta chiamata a Log.v() nella funzione onCreate della MainActivity.

--- app/src/main/java/com/devfabiocirelli/spaceinvaders/MainActivity.kt
+++ app/src/main/java/com/devfabiocirelli/spaceinvaders/MainActivity.kt
@@ -2,10 +2,14 @@ package com.devfabiocirelli.spaceinvaders
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.v(TAG, "onCreate")
     }
 }
